f5ed3de62a8bbe44c4990ed4899513136a51cade
hiddenblue <freewayrong@foxmail.com>
2024-11-30 21:14:02 +0800

    update the dependency and project info

--- timevar.py
+++ timevar.py
@@ -5,8 +5,8 @@ feedbacktime = 1.5
 
 
 class ProjectInfo:
-    VersionInfo = "1.1.5"
+    VersionInfo = "1.1.8"
     ProjectName = "Pubmedsoso"
-    LastUpdate = "20241123"
+    LastUpdate = "20241130"
     AuthorName = "hiddenblue"
 #此处的变量是main一次运行中需要多次调用的全局变量
